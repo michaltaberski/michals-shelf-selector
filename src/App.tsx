4af716d30d920d7a0b06076bc20bf897bed755bc
Michał Taberski <m.taberski@gmail.com>
2023-12-20 10:14:47 +0100

    Update layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,24 @@ import { LayoutBox } from "./components/LayoutBox";
 const App = () => {
   return (
     <>
+      <LayoutContainer className="mt-4">
+        <h1 className="text-3xl">Shelf Selector 🎉</h1>
+      </LayoutContainer>
       <LayoutContainer>
         <LayoutBox className="w-3/4"></LayoutBox>
         <LayoutBox className="w-1/4 min-w-64"></LayoutBox>
       </LayoutContainer>
       <LayoutContainer>
-        <div className="p-4 border-t w-full text-sm">
-          Michal's Shelf Selector
+        <div className="p-4 border-t w-full text-sm text-slate-500">
+          Michal's Shelf Selector. Images from{" "}
+          <a
+            href="https://unsplash.com"
+            target="_blank"
+            className="text-amber-500 underline  hover:no-underline"
+          >
+            https://unsplash.com
+          </a>
+          .
         </div>
       </LayoutContainer>
     </>
--- src/components/LayoutContainer.tsx
+++ src/components/LayoutContainer.tsx
@@ -1,9 +1,15 @@
+import { cn } from "../utils";
+
 export const LayoutContainer = ({
   children,
+  className,
 }: {
   children?: React.ReactNode;
+  className?: string;
 }) => {
   return (
-    <div className="p-4 mx-auto max-w-7xl flex flex-row gap-4">{children}</div>
+    <div className={cn("p-4 mx-auto max-w-7xl flex flex-row gap-4", className)}>
+      {children}
+    </div>
   );
 };
